1d6ae0cb4961729e0a265be50bba77918fbc2255
Alexander Hasslund <alexander.hasslund@stud.sti.se>
2023-10-24 22:48:41 +0200

    structure and game design just in text

---
+++ src/com/alexanderhasslund/demo/main/Main.java
@@ -0,0 +1,57 @@
+package com.alexanderhasslund.demo.main;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        // Player
+            // name
+            // experience
+            // stats - agility / intellect / strength
+                        // rage / energy / mana <- belong to Classes
+            // HP
+            // Currency
+            // Initiative?
+            // Damage
+                // List for items
+                // List for talent tree
+
+        // Monster
+            // stats - agility / intellect / strength
+                   // rage / energy / mana <- belong to Classes
+            // HP
+            // Initiative?
+            // Damage
+
+        // Classes
+            // Barb
+            // Thief <- extra loot? Everyone should have its own trait
+            // Warlock
+
+        // Items
+            // Base item
+            // mid-tier item
+            // late-game item
+            // divine item
+            // random out stats? <- reroll item?
+
+        // Levels
+            // random per map
+            // harder each level
+            // new game + when game over
+
+        // Shop
+            // secret shop / per level
+
+        // Rewards
+
+        // Talent tree
+
+        // ICombat
+
+        //write to file
+
+    }
+}
