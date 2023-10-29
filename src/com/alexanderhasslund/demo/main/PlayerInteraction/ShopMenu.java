426668ad756628853e7567deefd547dc8dd90ee8
Alexander Hasslund <alexander.hasslund@stud.sti.se>
2023-10-29 22:18:58 +0100

    pushing before messing with new functions

--- src/com/alexanderhasslund/demo/main/PlayerInteraction/ShopMenu.java
+++ src/com/alexanderhasslund/demo/main/PlayerInteraction/ShopMenu.java
@@ -1,21 +1,26 @@
 package com.alexanderhasslund.demo.main.PlayerInteraction;
 
 import com.alexanderhasslund.demo.main.Engine.Input;
+import com.alexanderhasslund.demo.main.Inventory.Inventory;
 import com.alexanderhasslund.demo.main.Player.Player;
 import com.alexanderhasslund.demo.main.Player.RegisterPlayer;
 
+import java.util.List;
+
 public class ShopMenu {
-
-
 
     public void basicShopMenu() {
         RegisterPlayer registerPlayer = new RegisterPlayer();
         Player player = new Player();
-        switch(Input.intInput()){
-            case 1 -> {registerPlayer.getPlayerList();}
+        switch(Input.intInput()) {
+            case 1 -> {
+                registerPlayer.getPlayerList();
+                registerPlayer.getPlayerList().;
+            }
             case 2 -> {
                 player.getInventoryList();
                 //player.setInventoryList();
+
             }
         }
     }
